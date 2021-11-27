94357ab240d22d462643af856bd5bb56c0e619c1
teresacabell <teresacabll@gmail.com>
2021-11-27 14:09:08 -0600

    made a confirm password length

--- script.js
+++ script.js
@@ -7,17 +7,26 @@ var specialChars = "#$%&'()*+,-./:;<=>?@[]^_`{|}~"
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  var passwordPrompt = parseInt(
-    window.prompt("Choose a password length from 8-128 characters.")
-  );
 
-  if passwordPrompt >= 8 && passwordPrompt <= 128
-    var confirmUpperChars = confirm("Do you want to include upper case letters?");
-    var confirmLowerChars = confirm("Do you want to include lower case letters?");
-    var confirmNumberChars = confirm("Do you want to include numbers?");
-    var confirmSpecialChars = confirm("Do you want to include special characters?");
+function generatePassword() {
+
+    var passwordPrompt = parseInt(
+      window.prompt("Choose a password length from 8-128 characters.")
+    );
+    console.log(passwordPrompt)
+
+    // check and alert if the password length chosen is out of bounds 
+    if (passwordPrompt < 8 || passwordPrompt > 128 || !passwordPrompt)
+      (alert("Your chosen length is out of parameters, please choose a number between 8-128."))
+    return generatePassword();
+
+    if (passwordPrompt >= 8 && passwordPrompt <= 128)
+      var confirmUpperChars = confirm("Do you want to include upper case letters?");
+      var confirmLowerChars = confirm("Do you want to include lower case letters?");
+      var confirmNumberChars = confirm("Do you want to include numbers?");
+      var confirmSpecialChars = confirm("Do you want to include special characters?");
 }
+
 
 // Write password to the #password input
 function writePassword() {
