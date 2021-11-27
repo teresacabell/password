1f3c4fd48f2693cec94cb8ac9bb5ae5de9f1f3bb
teresacabell <teresacabll@gmail.com>
2021-11-27 14:56:23 -0600

    got everything to work

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # password
+
+Worked in a study group with our TA Cody Herbert, Logan Scott, Sem Tristan, Jason Vissage and Cory John Thompson. 
+
+
+
--- script.js
+++ script.js
@@ -1,36 +1,69 @@
 // Assignment code here
-var lowerChars = "abcdefgheijklmnopqrstuvwxyz"
-var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var numberChars = "0123456789"
-var specialChars = "#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+var lowerChars = "abcdefgheijklmnopqrstuvwxyz";
+var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberChars = "0123456789";
+var specialChars = "#$%&'()*+,-./:;<=>?@[]\"\\^_`{|}~";
+var charCollector = "";
+var passwordCollector = "";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-
 function generatePassword() {
+  var passwordLength = parseInt(
+    window.prompt("Choose a password length from 8-128 characters.")
+  );
+  console.log(passwordLength);
 
-    var passwordPrompt = parseInt(
-      window.prompt("Choose a password length from 8-128 characters.")
+  // check and ask what kind of chars
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    var confirmUpperChars = confirm(
+      "Do you want to include upper case letters?"
     );
-    console.log(passwordPrompt)
+    console.log(confirmUpperChars);
+    if (confirmUpperChars) {
+      charCollector += upperChars;
+    }
+    var confirmLowerChars = confirm(
+      "Do you want to include lower case letters?"
+    );
+    console.log(confirmLowerChars);
+    if (confirmLowerChars) {
+      charCollector += lowerChars;
+    }
+    var confirmNumberChars = confirm("Do you want to include numbers?");
+    console.log(confirmNumberChars);
+    if (confirmNumberChars) {
+      charCollector += numberChars;
+    }
+    var confirmSpecialChars = confirm(
+      "Do you want to include special characters?"
+    );
+    console.log(confirmSpecialChars);
+    if (confirmSpecialChars) {
+      charCollector += specialChars;
+    } else if (
+      confirmUpperChars === false &&
+      confirmLowerChars === false &&
+      confirmSpecialChars === false &&
+      confirmNumberChars === false
+    ) {
+      alert("Must select at least one character type!");
+      return generatePassword();
+    }
 
-    // check and alert if the password length chosen is out of bounds 
-    if (passwordPrompt < 8 || passwordPrompt > 128 || !passwordPrompt)
-      (alert("Your chosen length is out of parameters, please choose a number between 8-128."))
+    for (var i=0; i < passwordLength; i++) {
+      passwordCollector += charCollector [Math.floor(Math.random() * charCollector.length)];
+    }
+    return passwordCollector;
+    
+  } else {
+    alert(
+      "Your chosen length is out of parameters, please choose a number between 8 and 128."
+    );
     return generatePassword();
-
-    if (passwordPrompt >= 8 && passwordPrompt <= 128)
-      var confirmUpperChars = confirm("Do you want to include upper case letters?");
-        console.log(confirmUpperChars)
-      var confirmLowerChars = confirm("Do you want to include lower case letters?");
-        console.log(confirmLowerChars)
-      var confirmNumberChars = confirm("Do you want to include numbers?");
-        console.log(confirmNumberChars)
-      var confirmSpecialChars = confirm("Do you want to include special characters?");
-        console.log(confirmSpecialChars)
+  }
 }
-
 
 // Write password to the #password input
 function writePassword() {
@@ -38,7 +71,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
